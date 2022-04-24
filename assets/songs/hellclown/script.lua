0a5f7a9365866609e5575933f20789ac3ddd43dc
Rednosus <103311757+Rednosus@users.noreply.github.com>
2022-04-24 10:13:11 +0700

    Add files via upload

--- assets/songs/hellclown/script.lua
+++ assets/songs/hellclown/script.lua
@@ -1,1 +1,19 @@
+local allowCountdown = false
+function onStartCountdown()
+	if not allowCountdown and isStoryMode and not seenCutscene then --Block the first countdown
+		startVideo('hellclownCutscene');
+		allowCountdown = true;
+		return Function_Stop;
+	end
+	return Function_Continue;
+end
 
+function onUpdate(elapsed)
+	for i = 0, getProperty('unspawnNotes.length')-1 do
+		--Check if the note is a Hurt Note
+		if getPropertyFromGroup('unspawnNotes', i, 'noteType') == 'Hurt Note' then
+			setPropertyFromGroup('unspawnNotes', i, 'texture', 'customNotes/FIRENOTE_assets'); --Change texture
+			setPropertyFromGroup('unspawnNotes', i, 'noteSplashTexture', 'customNotes/FIREnoteSplashes');
+		end
+	end
+end
